7fce502b5de5cbcb5fa7dc02630f36a2a2927a41
_Kaagaaz._ <290294969+Kaagaaz@users.noreply.github.com>
2026-07-30 09:03:04 +0530

    Update Destination.kt

--- app/java+kotlin/com.example.waypointv12/UI/Navigation/Destination.kt
+++ app/java+kotlin/com.example.waypointv12/UI/Navigation/Destination.kt
@@ -1,1 +1,17 @@
+package com.example.waypointv12.ui.navigation
 
+import kotlinx.serialization.Serializable
+
+sealed interface Destination {
+    @Serializable
+    data object Dashboard : Destination
+
+    @Serializable
+    data object Menu : Destination
+
+    @Serializable
+    data object ThreatLog : Destination
+
+    @Serializable
+    data object Settings : Destination
+}
